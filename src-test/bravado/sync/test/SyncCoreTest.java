a3ca61acfe1487b1888fdfb12d2465dd27bef385
Roberto Martins <miles.chet@gmail.com>
2012-06-02 15:13:01 -0300

    initial project

---
+++ src-test/bravado/sync/test/SyncCoreTest.java
@@ -0,0 +1,21 @@
+package bravado.sync.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * SyncCoreTest
+ * bob
+ * 6/2/12 1:39 PM
+ */
+
+public class SyncCoreTest {
+
+    @Test
+    public void testReposirotyGetAllFiles() {
+
+        Assert.assertEquals(1, 1);
+
+    }
+
+}
---
+++ src/bravado/sync/core/SyncCore.java
@@ -0,0 +1,25 @@
+package bravado.sync.core;
+
+import com.twmacinta.util.MD5;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * SyncCore
+ * bob
+ * 6/2/12 1:24 PM
+ */
+public class SyncCore {
+
+    private static List<String> sync(String repository, HashMap<String, String> listFilesRepository1) {
+
+        HashMap<String, String> listFilesServer = new HashMap<String, String>();
+
+        // listFilesRepository1 - repository // download
+
+        return null;
+    }
+
+}
