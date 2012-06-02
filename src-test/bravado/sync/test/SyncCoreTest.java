156c791800409f0f1b9a8cd28776ba6f131512e9
Roberto Martins <miles.chet@gmail.com>
2012-06-02 18:52:58 -0300

    initial test

--- src-test/bravado/sync/test/SyncCoreTest.java
+++ src-test/bravado/sync/test/SyncCoreTest.java
@@ -32,4 +32,25 @@ public class SyncCoreTest {
         Assert.assertEquals(operations.get(0).getOperation(), PUT);
     }
 
+    public void testRepo1PutAndGetFile2() {
+
+        MockSyncToken repo1 = new MockSyncToken("repo1", "~/");
+        MockSyncToken repo2 = new MockSyncToken("repo2", "~/");
+
+        MockServer server = new MockServer();
+        server.addFile(repo2, new FileEntry("file3.txt", "8343"));
+    
+        // repo1 add file
+        repo1.addFile(new FileEntry("file2.txt", "2343"));
+        
+        List<SyncOperation> operationsRepo1 = server.sync(repo1, repo1.repositoryFiles);
+
+        List<SyncOperation> operationsExpected = new ArrayList<SyncOperation>(); 
+        operationsExpected.add(new SyncOperation(new FileEntry("file3.txt", "8343"), GET));
+        operationsExpected.add(new SyncOperation(new FileEntry("file2.txt", "2343"), PUT));
+        
+        Assert.assertTrue(operationsRepo1.containsAll(operationsExpected));
+    }
+
+
 }
